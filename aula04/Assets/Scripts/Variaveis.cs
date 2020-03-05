f06639397a6fb210e6f6ae93de9ed4e44f738516
prof-salu <saluorodrigues@gmail.com>
2020-03-05 10:05:24 -0300

    arquivos aula04

---
+++ aula04/Assets/Scripts/Variaveis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour
+{
+    public int energia = 100;
+    public string nome = "Heroi";
+    public double forca = 45;
+    public bool estaVivo = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Iniciando jogo");
+        Debug.Log("Nome: " + nome);
+        Debug.Log("Energia: " + energia);
+        Debug.Log("Força: " + forca);
+        Debug.Log("Esta vivo: " + estaVivo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Comentario de linha
+
+        /*  comentario
+            de varias
+            linhas
+         */
+        //Debug.Log("executa a cada frame");
+    }
+}
