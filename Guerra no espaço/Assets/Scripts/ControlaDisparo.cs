b9f6826650190bd28ed966b9eebf6d194ca78f9e
salustiano oliveira <saluorodrigues@gmail.com>
2020-04-02 13:04:59 -0300

    arquivos da aula08
    - ControlaDisparo
    - ControlaLaser
    - AudioSource
    - Instantiate
    - Destroy
    - Prefabs

---
+++ Guerra no espaço/Assets/Scripts/ControlaDisparo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaDisparo : MonoBehaviour
+{
+    public GameObject laser;
+    public float atrasoNoDisparo = 0.5f;
+    private float timerDisparo = 0;
+    void Update()
+    {
+        timerDisparo -= Time.deltaTime;
+        
+        if (Input.GetButtonDown("Fire1") && timerDisparo <= 0)
+        {
+            //Debug.Log(Time.deltaTime);
+            //Botao esquerdo do mouse
+            //Debug.Log("Fire 1");
+            //laser.SetActive(false);
+            //Instantiate(gameobject, posicao, rotacao);
+
+            timerDisparo = atrasoNoDisparo;
+            Instantiate(laser, transform.position, transform.rotation);
+
+
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //Botao direito do mouse
+            //Debug.Log("Fire 2");
+            //laser.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            //Botao do meio do mouse
+            //Debug.Log("Fire3");
+        }
+
+    }
+}
---
+++ Guerra no espaço/Assets/Scripts/ControlaLaser.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaLaser : MonoBehaviour
+{
+    public float velMax = 7f;
+    private float limiteDolaser = 0.5f;
+    private float duracaoDisparo = 1.2f;
+
+    void Update()
+    {
+        Vector3 posicao = transform.position;
+
+        Vector3 velocidade = new Vector3(0, velMax * Time.deltaTime, 0);
+
+        posicao += transform.rotation * velocidade;
+
+        //Limite vertical
+
+        //Topo
+        if(posicao.y - limiteDolaser > Camera.main.orthographicSize)
+        {
+            //Debug.Log("Laser fora da cena (TOPO)");
+            //Destroy(gameobjectDestruido);
+            //Destroy(gameobjectDestruido, tempoAtraso);
+            Destroy(this.gameObject, duracaoDisparo);
+        }
+
+        //Baixo
+        if(posicao.y + limiteDolaser < -Camera.main.orthographicSize)
+        {
+            //Debug.Log("Laser fora da cena (BAIXO)");
+            Destroy(this.gameObject, duracaoDisparo);
+        }
+
+        
+        //Proporcao da cena
+        float proporcaoCena = (float)Screen.width / (float)Screen.height;
+        //Largura da camera
+        float larguraCamera = Camera.main.orthographicSize * proporcaoCena;
+
+        //Limite horizontal
+        //Esquerda
+        if (posicao.x - limiteDolaser > larguraCamera)
+        {
+            Destroy(this.gameObject, duracaoDisparo);
+        }
+
+        //Direita
+        if(posicao.x + limiteDolaser < -larguraCamera)
+        {
+            Destroy(this.gameObject, duracaoDisparo);
+        }
+
+        transform.position = posicao;
+    }
+}
